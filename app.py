de6c0adb9f30bc32360e851640f3e175c427be50
davidberiro <david.benchimol@mail.huji.ac.il>
2017-12-05 15:51:06 +0200

    kicking me out of micosoft lab..

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/creategame', methods=['POST'])
+def creategame():
+    username = request.form['username']
+    return username
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
