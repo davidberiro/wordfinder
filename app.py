aeb5fbbf76f547584853ffef2de55d5a7a5a73b0
David <deebenchimol@gmail.com>
2017-12-06 13:38:04 +0200

    working

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/creategame/', methods=['POST'])
+def creategame():
+    username = request.form.get('username')
+    return render_template('game.html', name = username)
 
 if __name__ == '__main__':
     app.run(debug=True)
